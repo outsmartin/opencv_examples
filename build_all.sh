f10288ae8e5319a59df1eda02d1a9517858b14e7
Martin Schneider <info@outsmartin.de>
2012-07-02 18:01:21 +0200

    reorganized

--- build_all.sh
+++ build_all.sh
@@ -2,5 +2,5 @@
 
 for i in *.cpp; do
   echo "compiling $i"
-  g++ -ggdb `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
+  g++ -ggdb `pkg-config --cflags opencv` -o bin/`basename $i .cpp` $i `pkg-config --libs opencv`;
 done
